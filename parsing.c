db9aed5a49fd6d0e0a1afaee7e2f4b457b1eae2b
Pavel Gurkov <true.neu@gmail.com>
2025-09-06 16:17:00 +0100

    Evaluation

--- parsing.c
+++ parsing.c
@@ -7,12 +7,48 @@
 
 #define INPUT_BUFFER_LENGTH 2048
 
+long eval_op(long x, char *op, long y) {
+  if (strcmp(op, "+") == 0) {
+    return x + y;
+  }
+  if (strcmp(op, "-") == 0) {
+    return x - y;
+  }
+  if (strcmp(op, "/") == 0) {
+    return x / y;
+  }
+  if (strcmp(op, "*") == 0) {
+    return x * y;
+  }
+  // should never reach here
+  return 0;
+};
 
+long eval(mpc_ast_t *t) {
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  // the operator is always second child
+  char *op = t->children[1]->contents;
+
+  // store third child in x
+  long x = eval(t->children[2]);
+
+  // complete the rest of children
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
 
 int main(int argc, char **argv) {
-  mpc_parser_t* Number = mpc_new("number");
-  mpc_parser_t* Operator = mpc_new("operator");
-  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t *Number = mpc_new("number");
+  mpc_parser_t *Operator = mpc_new("operator");
+  mpc_parser_t *Expr = mpc_new("expr");
   mpc_parser_t *Lispy = mpc_new("lispy");
 
   mpca_lang(MPCA_LANG_DEFAULT, " \
@@ -26,12 +62,13 @@ int main(int argc, char **argv) {
   puts("Lispy version 0.0.1");
 
   while (1) {
-    char* input = readline("lispy> ");
+    char *input = readline("lispy> ");
     add_history(input);
 
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       mpc_err_print(r.error);
